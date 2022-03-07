dce80a67c244e145b2fcce14b00c78c3225217a4
Carlos Daniel <killer.dk20@gmail.com>
2022-03-07 12:34:57 -0300

    chore: add message

--- src/App.tsx
+++ src/App.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 interface AppProps {}
 
 export const App: React.FC<AppProps> = () => {
-  return <h1>Olá, eu sou Carlos Daniel!</h1>
+  return <h1>Olá, eu sou Carlos Daniel!, logo logo vai ter um site aqui 😁</h1>
 };
