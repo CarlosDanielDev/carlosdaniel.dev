27faffb08efa0fe2013ca1da30eef5adaa3433b6
Carlos Daniel <killer.dk20@gmail.com>
2022-03-15 20:33:40 -0300

    chore: create container centered

---
+++ src/components/Container/index.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const CenteredContainer = styled.div`
+	max-width: 900px;
+	width: 100%;
+	overflow: hidden;
+	display: flex;
+	flex-direction: column;
+	margin: 0 auto;
+	padding: 0 10px;
+	height: 100vh;
+
+	@media (max-width: 800px) {
+		padding: 0;
+	}
+`;
