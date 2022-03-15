5017261b428e632e0d644417d569134d82f84e4f
Carlos Daniel <killer.dk20@gmail.com>
2022-03-15 20:32:02 -0300

    chore: add eslint & prettier and libs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import alias from '@rollup/plugin-alias';
 
-// https://vitejs.dev/config/
+const src = resolve(__dirname, 'src');
+const components = resolve(__dirname, 'src/components');
+
 export default defineConfig({
-  plugins: [react()]
-})
+	plugins: [
+		react(),
+		alias({
+			entries: [
+				{ find: 'src', replacement: src },
+				{ find: 'components', replacement: components },
+			],
+		}),
+	],
+});
