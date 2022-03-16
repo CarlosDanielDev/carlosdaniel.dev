cbd06c3e159b532b8f63c3f873c7c7aa801c74dc
Carlos Daniel <killer.dk20@gmail.com>
2022-03-16 02:55:04 -0300

    chore: separate colors from theme

---
+++ src/contexts/theme/dark.ts
@@ -0,0 +1,14 @@
+import { colors } from './colors';
+
+export const dark = {
+	title: 'dark',
+	typography: {
+		background: colors.charlestonGreen,
+		backgroundHover: colors.outerSpace,
+		title: colors.white,
+		primary: colors.brandeisBlue,
+		textColorPrimary: colors.white,
+		secondary: colors.manatee,
+		placeholder: colors.sonicSilver,
+	},
+};
---
+++ src/contexts/theme/light.ts
@@ -0,0 +1,14 @@
+import { colors } from './colors';
+
+export const light = {
+	title: 'light',
+	typography: {
+		background: colors.ghostWhite,
+		backgroundHover: colors.white,
+		title: colors.outerSpace,
+		primary: colors.brandeisBlue,
+		textColorPrimary: colors.white,
+		secondary: colors.lightGrey,
+		placeholder: colors.sonicSilver,
+	},
+};
