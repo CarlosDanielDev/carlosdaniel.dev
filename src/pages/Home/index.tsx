9ae606779992ca097975fbaf47eefcc11547edbe
Carlos Daniel <killer.dk20@gmail.com>
2022-03-16 18:48:46 -0300

    chore: add title for acessibility

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,14 +4,12 @@ import * as S from './styles';
 interface HomeProps {}
 
 export const Home: React.FC<HomeProps> = () => {
+	const disclaimer =
+		'Olá, é um prazer em te encontrar por aqui, mas eu ainda estou construindo essa página, enquanto isso você pode navegar para a página de links pra me encontrar nas redes sociais, vlw flw ✌️...';
 	return (
 		<S.Wrapper>
-			<S.Disclaimer>
-				<S.Paragraph>
-					Olá, é um prazer em te encontrar por aqui, mas eu ainda estou
-					construindo essa página, enquanto isso você pode navegar para a página
-					de links pra me encontrar nas redes sociais, vlw flw ✌️...
-				</S.Paragraph>
+			<S.Disclaimer title={disclaimer}>
+				<S.Paragraph title={disclaimer}>{disclaimer}</S.Paragraph>
 			</S.Disclaimer>
 		</S.Wrapper>
 	);
