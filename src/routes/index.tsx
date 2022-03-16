24627ffbe37c3f95ba47bd0c12ffac6d36c4a91b
Carlos Daniel <killer.dk20@gmail.com>
2022-03-16 18:42:37 -0300

    chore: create default components to nes pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Routes as Switch, Route } from 'react-router-dom';
+import {
+	BrowserRouter,
+	Routes as Switch,
+	Route,
+	Outlet,
+} from 'react-router-dom';
+import { Header, Footer, CenteredContainer } from 'components';
 import * as P from '../pages';
 
 interface RoutesProps {}
@@ -7,9 +13,22 @@ interface RoutesProps {}
 export const Routes: React.FC<RoutesProps> = () => (
 	<BrowserRouter>
 		<Switch>
-			<Route path="/" element={<P.Home />} />
-			<Route path="/to-japanese" element={<P.ToJapanese />} />
-			<Route path="/links" element={<P.Links />} />
+			<Route
+				path="/"
+				element={
+					<div style={{ maxHeight: '100vh' }}>
+						<CenteredContainer>
+							<Header />
+							<Outlet />
+						</CenteredContainer>
+						<Footer />
+					</div>
+				}
+			>
+				<Route path="/" element={<P.Home />} />
+				<Route path="/to-japanese" element={<P.ToJapanese />} />
+				<Route path="/links" element={<P.Links />} />
+			</Route>
 		</Switch>
 	</BrowserRouter>
 );
