0e4db4e472d3a956b3cb48c1af2d4916d7d8754e
Carlos Daniel <killer.dk20@gmail.com>
2022-03-16 18:43:24 -0300

    chore: reduce landscape breakpoint to 1020

--- src/constants/breakpoints.ts
+++ src/constants/breakpoints.ts
@@ -1,5 +1,5 @@
 export const breakpointIphoneDevices = 320;
 export const breakpointAndroidDevices = 480;
 export const breakpointMobile = 801;
-export const breakpointLandscape = 1025;
+export const breakpointLandscape = 1020;
 export const breakpointDesktop = 1281;
