e68223e4b0a7cf05c1aa5b58b5147493dd946e7f
Carlos Daniel <killer.dk20@gmail.com>
2022-03-15 21:01:10 -0300

    chore: add link top redirect

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { CenteredContainer } from 'components';
 import * as S from './styles';
+import { Link } from 'react-router-dom';
 
 interface HomeProps {}
 
-export const Home: React.FC<HomeProps> = () => (
-	<CenteredContainer>
-		<S.Wrapper>Oi</S.Wrapper>
-	</CenteredContainer>
-);
+export const Home: React.FC<HomeProps> = () => {
+	return (
+		<CenteredContainer>
+			<S.Wrapper>
+				<Link to="/links">Links</Link>
+			</S.Wrapper>
+		</CenteredContainer>
+	);
+};
