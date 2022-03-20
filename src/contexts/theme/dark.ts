0c6ca47bb2b56127f461c4b2e7cd31e6dbc9cf30
Carlos Daniel <killer.dk20@gmail.com>
2022-03-20 12:24:20 -0300

    style: add new color to themes dark/light

--- src/contexts/theme/dark.ts
+++ src/contexts/theme/dark.ts
@@ -2,7 +2,7 @@ import { colors } from './colors';
 
 export const dark = {
 	title: 'dark',
-	typography: {
+	palette: {
 		background: colors.charlestonGreen,
 		backgroundHover: colors.outerSpace,
 		title: colors.white,
@@ -11,5 +11,6 @@ export const dark = {
 		secondary: colors.manatee,
 		placeholder: colors.sonicSilver,
 		shadow: 'rgb(0 0 0 / 10%) 0px 1px 3px 0px, rgb(0 0 0 / 6%) 0px 1px 2px 0px',
+		fadedShadow: `0px 22px 119px 0px ${colors.outerSpace75}`,
 	},
 };
--- src/contexts/theme/light.ts
+++ src/contexts/theme/light.ts
@@ -2,7 +2,7 @@ import { colors } from './colors';
 
 export const light = {
 	title: 'light',
-	typography: {
+	palette: {
 		background: colors.ghostWhite,
 		backgroundHover: colors.white,
 		title: colors.outerSpace,
@@ -11,5 +11,6 @@ export const light = {
 		secondary: colors.spanishGrey,
 		placeholder: colors.sonicSilver,
 		shadow: 'rgb(0 0 0 / 10%) 0px 1px 3px 0px, rgb(0 0 0 / 6%) 0px 1px 2px 0px',
+		fadedShadow: `0px 22px 119px 0px ${colors.white75}`,
 	},
 };
