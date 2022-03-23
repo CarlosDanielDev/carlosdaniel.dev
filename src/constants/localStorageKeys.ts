5cd41219c6050346b85413b4da41e361f2cc3e26
Carlos Daniel <killer.dk20@gmail.com>
2022-03-23 06:12:04 -0300

    style: create new constants

---
+++ src/constants/localStorageKeys.ts
@@ -0,0 +1,4 @@
+const prefix = '@carlosdaniel.dev::';
+
+export const routesTaskKeyName = `${prefix}:task-routes`;
+export const dataKeyNamePreset = `${prefix}data-`; // this will be like this on LS @carlosdaniel.dev::data-something-else
