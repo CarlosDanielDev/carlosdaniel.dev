73e832c8c535946841a8470d78754badb05598e2
Carlos Daniel <killer.dk20@gmail.com>
2022-03-23 06:15:35 -0300

    feat: create useEventListener hook

---
+++ src/hooks/useEventListener/index.tsx
@@ -0,0 +1,37 @@
+import { RefObject, useEffect, useRef } from 'react';
+
+import { useIsomorphicLayoutEffect } from 'src/hooks';
+
+export function useEventListener<
+	KW extends keyof WindowEventMap,
+	KH extends keyof HTMLElementEventMap,
+	T extends HTMLElement | void = void,
+>(
+	eventName: KW | KH,
+	handler: (
+		event: WindowEventMap[KW] | HTMLElementEventMap[KH] | Event,
+	) => void,
+	element?: RefObject<T>,
+) {
+	const savedHandler = useRef(handler);
+
+	useIsomorphicLayoutEffect(() => {
+		savedHandler.current = handler;
+	}, [handler]);
+
+	useEffect(() => {
+		const targetElement: T | Window = element?.current || window;
+		if (!(targetElement && targetElement.addEventListener)) {
+			return;
+		}
+
+		const eventListener: typeof handler = event => savedHandler.current(event);
+
+		targetElement.addEventListener(eventName, eventListener);
+
+		// eslint-disable-next-line consistent-return
+		return () => {
+			targetElement.removeEventListener(eventName, eventListener);
+		};
+	}, [eventName, element]);
+}
