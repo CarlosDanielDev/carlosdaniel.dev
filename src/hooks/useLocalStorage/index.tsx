4661fa2bded551d29d2f7f09211f3e86c999d699
Carlos Daniel <killer.dk20@gmail.com>
2022-03-23 06:17:17 -0300

    refactor: improve useLocalSroeage hook to persist and be like react state

--- src/hooks/useLocalStorage/index.tsx
+++ src/hooks/useLocalStorage/index.tsx
@@ -1,21 +1,77 @@
-import { useEffect, useState } from 'react';
-import { light } from 'src/contexts/theme/light';
+import {
+	Dispatch,
+	SetStateAction,
+	useCallback,
+	useEffect,
+	useState,
+} from 'react';
+import { parseJSON } from 'src/utils';
 
-type Theme = typeof light;
+import { useEventListener } from 'src/hooks';
 
-export function useLocalStorage(key: string, initialState: Theme) {
-	const [state, setState] = useState(() => {
-		const storageValue = localStorage.getItem(key);
+declare global {
+	interface WindowEventMap {
+		'local-storage': CustomEvent;
+	}
+}
 
-		if (storageValue) {
-			return JSON.parse(storageValue);
+type SetValue<T> = Dispatch<SetStateAction<T>>;
+
+export function useLocalStorage<T>(
+	key: string,
+	initialValue: T,
+): [T, SetValue<T>] {
+	const readValue = useCallback((): T => {
+		if (typeof window === 'undefined') {
+			return initialValue;
 		}
-		return initialState;
-	});
+
+		try {
+			const item = window.localStorage.getItem(key);
+			return item ? (parseJSON(item) as T) : initialValue;
+		} catch (error) {
+			console.warn(`Error reading localStorage key “${key}”:`, error);
+			return initialValue;
+		}
+	}, [initialValue, key]);
+
+	const [storedValue, setStoredValue] = useState<T>(readValue);
+
+	const setValue: SetValue<T> = useCallback(
+		value => {
+			if (typeof window === 'undefined') {
+				console.warn(
+					`Tried setting localStorage key “${key}” even though environment is not a client`,
+				);
+			}
+
+			try {
+				const newValue = value instanceof Function ? value(storedValue) : value;
+
+				window.localStorage.setItem(key, JSON.stringify(newValue));
+
+				setStoredValue(newValue);
+
+				window.dispatchEvent(new Event('local-storage'));
+			} catch (error) {
+				console.warn(`Error setting localStorage key “${key}”:`, error);
+			}
+		},
+		[key, storedValue],
+	);
 
 	useEffect(() => {
-		localStorage.setItem(key, JSON.stringify(initialState));
-	}, [key, initialState]);
+		setStoredValue(readValue());
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 
-	return [state, setState];
+	const handleStorageChange = useCallback(() => {
+		setStoredValue(readValue());
+	}, [readValue]);
+
+	useEventListener('storage', handleStorageChange);
+
+	useEventListener('local-storage', handleStorageChange);
+
+	return [storedValue, setValue];
 }
