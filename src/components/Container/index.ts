c80f2ff188e845730926f95a7642ba50aa874871
Carlos Daniel <killer.dk20@gmail.com>
2022-03-23 06:08:40 -0300

    style: change conatiner height and width

--- src/components/Container/index.ts
+++ src/components/Container/index.ts
@@ -1,15 +1,17 @@
+import { breakpointLandscape } from 'src/constants';
 import styled from 'styled-components';
 
 export const CenteredContainer = styled.div`
-	max-width: 900px;
 	width: 100%;
+	height: 100%;
 	overflow: hidden;
 	display: flex;
 	flex-direction: column;
 	margin: 0 auto;
 	padding: 0 10px;
 
-	@media (max-width: 800px) {
+	@media (min-width: ${breakpointLandscape}px) {
+		max-width: 900px;
 		padding: 0;
 	}
 `;
