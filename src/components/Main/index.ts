391b08c31c9611b9087ea8bd4cab149eed585551
Carlos Daniel <killer.dk20@gmail.com>
2022-03-23 06:10:35 -0300

    style: change Main height

--- src/components/Main/index.ts
+++ src/components/Main/index.ts
@@ -2,6 +2,7 @@ import { breakpointDesktop } from 'src/constants';
 import styled from 'styled-components';
 
 export const Main = styled.main`
+	height: 100%;
 	@media (min-width: ${breakpointDesktop}px) {
 		padding-top: 80px;
 	}
