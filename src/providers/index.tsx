270123d4b8c3be469888c62b448522ce9a8d5f4d
Carlos Daniel <carlosdanieldev@gmail.com>
2025-04-26 19:53:16 -0300

    feat: add optional `children` prop to `RootProvider` for improved component flexibility

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { MobileProvider, Theme, HeaderProvider } from 'src/contexts';
 
-interface RootProviderProps {}
+interface RootProviderProps {
+	children?: React.ReactNode;
+}
 
 export const RootProvider: React.FC<RootProviderProps> = ({ children }) => {
 	const providers = [Theme, MobileProvider, HeaderProvider];
