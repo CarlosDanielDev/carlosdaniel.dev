a7371ce1ef0eb795d1e8f2a520c3bf5cf0466ae8
Carlos Daniel <carlosdanieldev@gmail.com>
2025-04-27 00:00:20 -0300

    chore: fix issues kebab case

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,6 @@
-export * from './home';
-export * from './to-japanese';
+export { Home } from './Home';
+export { ToJapanese } from './ToJapanese';
 export { Links } from './Links';
-export * from './not-found';
-export * from './terminal';
-export * from './about';
+export { NotFound } from './NotFound';
+export { Terminal } from './Terminal';
+export { About } from './About';
